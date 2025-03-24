f848545cebf90edcf408fffaa1e6dba1f3676290
Ethan Wee <Ethan.Wee@amd.com>
2025-03-24 07:51:33 -0700

    Update build_rocm.sh

--- manywheel/build_rocm.sh
+++ manywheel/build_rocm.sh
@@ -239,8 +239,8 @@ do_lightweight_build() {
     done
 
     # Set environment so build_common.sh (or build_libtorch.sh) sees it
-    DEPS_LIST=( "${ROCM_SO_PATHS_LIGHTWEIGHT[*}" )
-    DEPS_SONAME=( "${LIGHTWEIGHT_ROCM_SO_FILES[*]}" )
+    DEPS_LIST=(${ROCM_SO_PATHS_LIGHTWEIGHT[*})
+    DEPS_SONAME=(${LIGHTWEIGHT_ROCM_SO_FILES[*]})
     DEPS_AUX_SRCLIST=()
     DEPS_AUX_DSTLIST=()
 
