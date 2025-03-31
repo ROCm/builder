765e1d3e68c17c3d7c26bb96f4486b6dda7d9ed7
Jithun Nair <jithun.nair@amd.com>
2025-03-31 23:09:22 +0000

    Lint

--- manywheel/build_common.sh
+++ manywheel/build_common.sh
@@ -195,7 +195,7 @@ for pkg in /$WHEELHOUSE_DIR/torch_no_python*.whl /$WHEELHOUSE_DIR/${WHEELNAME_MA
     # replace original wheel
     rm -f $pkg
     mv $(basename $pkg) $pkg
-    // Rename wheel to match metadata in dist-info
+    # Rename wheel to match metadata in dist-info
     mv $pkg $(echo $pkg | sed -e "s/\.git/${WHEELNAME_MARKER}.git/")
     cd ..
     rm -rf tmp
