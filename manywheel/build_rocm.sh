e543923d578e5d8ec3027afb2e01265ae2e4d2e1
Prachi Gupta <pracgupt@amd.com>
2025-10-03 12:31:20 -0400

    remove rocm check for triton package

--- manywheel/build_rocm.sh
+++ manywheel/build_rocm.sh
@@ -358,7 +358,7 @@ if [ ${PYTORCH_VERSION%%\.*} -ge 2 ]; then
         TRITON_CONSTRAINT="platform_system == 'Linux' and platform_machine == 'x86_64'$(if [[ $(ver "$PYTORCH_VERSION") -le $(ver "2.5") ]]; then echo " and python_version < '3.13'"; fi)"
         # Use "triton" for dev builds, else "pytorch-triton-rocm"
 		# Temp: Currently enabling for rocm7.1_internal_testing branch only but plan to expand it to other branches
-        if [[ "$PYTORCH_VERSION_FULL" -ge $(ver 2.8) ]] && [[ $ROCM_INT -gt 70000 ]]; then
+        if [[ "$PYTORCH_VERSION_FULL" -ge $(ver 2.8) ]]; then
             PKG="triton"
         else
             PKG="pytorch-triton-rocm"
